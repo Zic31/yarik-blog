edceed3174dd5e04e942f9db6f316b6fae59d061
Your name Ivan <>
2022-10-04 12:36:28 +0100

    Change sqlite to postgres

--- main.py
+++ main.py
@@ -2,11 +2,17 @@ from flask import Flask, render_template, request, url_for, redirect, flash, sen
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import UserMixin, login_user, LoginManager, login_required, current_user, logout_user
-
+import os
 app = Flask(__name__)
 
-app.config['SECRET_KEY'] = 'secret-key-goes-here'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
+
+
+# DEPLOYED WEBSITE
+app.config['SECRET_KEY'] =  os.environ.get("SECRET_KEY")
+# app.config['SECRET_KEY'] = 'secret-key-goes-here'
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL")
+
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
